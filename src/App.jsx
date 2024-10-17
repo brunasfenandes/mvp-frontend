af225ad8b38622b818ecc600d3f0e63df9ab8d5b
Keerat <tanwarkeeratcs@gmail.com>
2024-10-17 17:45:26 -0600

    Create routes, HomePage folder, RoomPage folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.scss'
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import RoomPage from "./pages/RoomPage/RoomPage";
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/rooms" element={<RoomPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
