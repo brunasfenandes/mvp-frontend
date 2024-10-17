9e89e132f64cd0f09960d9fa56357dbe3bbfcbdd
Keerat <tanwarkeeratcs@gmail.com>
2024-10-17 17:53:44 -0600

    Add Header inside BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import RoomPage from "./pages/RoomPage/RoomPage";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/rooms" element={<RoomPage />} />
