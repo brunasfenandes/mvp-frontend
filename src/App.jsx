d054f843ba5944fab9195601d50970f538fdba92
brunasfenandes <bruna.fenandes@outlook.com>
2024-10-18 07:56:32 -0400

    Added rout fot NotFoundPage and NotFoundPage component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import RoomPage from "./pages/RoomPage/RoomPage";
 import { Header } from "./components/Header/Header";
 import ChatPage from "./pages/ChatPage/ChatPage";
+import NotFound from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/rooms/:id" element={<RoomPage />} />
         <Route path="/rooms/:id/chat" element={<ChatPage />} />
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   );
