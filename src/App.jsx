979d83aefebb643fa7041a8ea0e9a2367409150c
Paul Jones <bethekind@gmail.com>
2024-10-18 01:25:49 -0600

    Add Toxic check. Still need to test

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import RoomPage from "./pages/RoomPage/RoomPage";
 import { Header } from "./components/Header/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return (
-    <BrowserRouter>
+  return <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/rooms/:id" element={<RoomPage />} />
       </Routes>
-    </BrowserRouter>
-  );
+      <ToastContainer autoClose={3000} />
+    </BrowserRouter>;
 }
 
 export default App;
---
+++ src/utils/checkToxicity.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+/**
+ * Checks if a given message is toxic by making a request to the backend.
+ *
+ * @param {string} message - The message to evaluate.
+ * @returns {Promise<boolean>} - Returns true if the message is toxic, otherwise false.
+ */
+async function checkToxicity(message) {
+    try {
+        const response = await axios.post('/isMessageToxic', { message });
+        if (response.data.isToxic) {
+            toast.error('Your chat message was toxic and could be harmful to others. Please be respectful and kind.');
+        }
+        return response.data.isToxic;
+    } catch (error) {
+        console.error('Error checking toxicity:', error);
+        // In case of an error, default to non-toxic
+        return false;
+    }
+}
+
+export default checkToxicity;
