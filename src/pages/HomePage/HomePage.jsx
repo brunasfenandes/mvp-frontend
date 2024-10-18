ef7c654b1cea20ffaec9fb21009e09035295b9a3
Keerat <tanwarkeeratcs@gmail.com>
2024-10-18 01:45:36 -0600

    Add id to the cards

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,21 +3,22 @@ import Card from "../../components/Card/Card";
 
 function HomePage() {
   const cardData = [
-    { text: "People with Scrizophrenia", color: "lightsalmon" },
-    { text: "People with Depression", color: "#f0a7a0" },
-    { text: "People with OCD", color: "#f4d35e" },
-    { text: "Personality disorders", color: "lightsalmon" },
-    { text: "Problem Gambling", color: "#96bbbb" },
-    { text: "Substance dependency", color: "#f4d35e" },
+    { id: 1, text: "People with Scrizophrenia", color: "lightsalmon" },
+    { id: 2, text: "People with Depression", color: "#f0a7a0" },
+    { id: 3, text: "People with OCD", color: "#f4d35e" },
+    { id: 4, text: "Personality disorders", color: "lightsalmon" },
+    { id: 5, text: "Problem Gambling", color: "#96bbbb" },
+    { id: 6, text: "Substance dependency", color: "#f4d35e" },
   ];
 
   return (
     <div>
       HOMEPAGE
-      <Card text="HI" backgroundColor="blue" />
       <div className="card-list">
         {cardData.map((card) => {
-          return <Card text={card.text} backgroundColor={card.color} />;
+          return (
+            <Card text={card.text} backgroundColor={card.color} key={card.id} />
+          );
         })}
       </div>
     </div>
