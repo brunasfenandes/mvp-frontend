b1c89c62ba4de5d183836eea23c31d0829d48504
Okashe <okashabdi88@gmail.com>
2024-10-18 11:36:02 -0500

    remove unused files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import RoomPage from "./pages/RoomPage/RoomPage";
 import { Header } from "./components/Header/Header";
-import ChatPage from "./pages/ChatPage/ChatPage";
 import NotFound from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
@@ -13,8 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/rooms/:id" element={<RoomPage />} />
-        <Route path="/rooms/:id/chat" element={<ChatPage />} />
-        <Route path="*" element={<NotFound/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
